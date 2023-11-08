799bab7babfc2e43f9000bf71845ebe75edbddd0
shimu-7 <shamima19shimu@gmail.com>
2023-11-08 13:17:47 +0600

    category based job displayed based on click

---
+++ src/components/HomeJob.jsx
@@ -0,0 +1,28 @@
+import { PropTypes } from "prop-types";
+import { Link } from "react-router-dom";
+
+const HomeJob = ({job}) => {
+    const {poster,email,_id, title, category, photo, description, salary, deadline, applicants,pDate}= job
+    return (
+        <div>
+            {
+                <div className="card card-side bg-base-100 shadow-xl">
+                <figure><img className="max-w-sm h-60" src={photo} alt="Movie"/></figure>
+                <div className="card-body">
+                  <h2 className="card-title">{title}</h2>
+                  <p><span className="font-semibold">Posted by</span>: {poster}</p>
+                  <p><span className="font-semibold">Category</span>: {category}</p>
+                  <p><span className="font-semibold">Salary</span>: {salary}</p>
+                  <Link className="w-full" to={`/jobDetails/${_id}`}><button className="btn btn-outline btn-secondary">Views Details</button></Link>
+                </div>
+              </div>
+            }
+        </div>
+    );
+};
+
+HomeJob.propTypes = {
+    job: PropTypes.object
+}
+
+export default HomeJob;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,47 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import NavBar from "../shared/NavBar";
 import { Tab } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import Section1 from "../components/section1";
 import Section2 from "../components/Section2";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+import HomeJob from "../components/HomeJob";
 
 
 
 const Home = () => {
     const [categories, setCategories] = useState([])
+    const [showJobs, setShowJobs] = useState([])
+    const { loading } = useContext(AuthContext)
+    const allJobs = useLoaderData();
+    const [flag, setFlag] = useState("");
     useEffect(() => {
         fetch('http://localhost:5000/category')
             .then(res => res.json())
             .then(data => setCategories(data))
     }, [])
+
+    const handleSearch = e =>{
+        e.preventDefault();
+        const searchValue = e.target.job.value.toLowerCase();
+        console.log(searchValue);
+        const searchedJob= allJobs.filter(job=>job.title.toLowerCase().includes(searchValue));
+        console.log(searchedJob);
+        //setShowJobs(jobs);   
+        setShowJobs(searchedJob);   
+    }
+
+    const handleCategory = (category) => {
+        setFlag(category);
+        const jobs = allJobs.filter(job => job.category === category);
+        console.log(category);
+
+        console.log(allJobs, jobs);
+        setShowJobs(jobs)
+        setShowJobs(jobs)
+        console.log(showJobs);
+    }
     return (
         <div>
             <NavBar></NavBar>
@@ -24,7 +52,7 @@ const Home = () => {
                         <h1 className="mb-5 text-3xl text-white font-bold">Unlock Your Potential</h1>
                         <p>With a world of opportunities at your fingertips, we're here to guide you towards success. <br /> Let's make your aspirations a reality together.</p>
                         <div className="mt-5">
-                            <form className="w-full ">
+                            <form onSubmit={handleSearch} className="w-full ">
                                 <input type="text" placeholder="Enter Job Name" name="job" id="" className="py-3 mr-2 md:w-2/3 px-2 rounded-lg" />
                                 <input type="submit" value="Search" className=" btn bg-pink-700 text-white border py-3 rounded-lg " />
                             </form>
@@ -39,13 +67,22 @@ const Home = () => {
                     <h2 className="text-3xl text-center font-semibold mt-10 mb-5">Job Category</h2>
                     <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">
                         {
-                            categories.map(category => <Tab className="btn btn-outline px-5" key={category._id} 
-                             >
+                            categories.map(category => <Tab onClick={() => handleCategory(category.category)} className={`${(flag === category.category) ? "btn bg-pink-700 text-white" : "btn btn-outline"} px-5`} key={category._id}
+                            >
                                 {category.category}
                             </Tab>)
                         }
                     </div>
+                   
+                    <div className="grid grid-cols-1 my-10 md:grid-cols-2 gap-5">
+                        
+                        {
+                            showJobs?.map(job => <HomeJob key={job._id} job={job}></HomeJob>)
+                        }
+                    </div>
                 </div>
+
+
 
             </div>
 
